498b66dfb84444c4d2175ed951e6fe82746a79f2
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-01 14:01:11 +0700

    list and key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {array.map((item, index) => {
+        return <div key={index}>{item}</div>
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
