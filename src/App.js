3faed409d7226d2aaf2aac912dc4a0b05b7a48d0
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-01 14:33:33 +0700

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  // const [input, setInput] = useState('')
+  const [input2, setInput2] = useState([])
+
+  const [data, setData] = useState([
+    {
+      nama: '',
+      alamat: '',
+      nohp: '',
+    },
+  ])
+
+  const handlechange = (e) => {
+    const { value } = e.target
+    const { name } = e.target
+    setInput2({
+      ...input2,
+      [name]: value,
+    })
+  }
+
+  const handleData = (e) => {
+    const { value } = e.target
+    const { name } = e.target
+    setData({
+      ...data,
+      [name]: value,
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="">
+        input2 :
+        <input type="text" name="input2" onChange={handlechange} />
+        <br />
+        input3 :
+        <input type="text" name="input3" onChange={handlechange} />
+        <br />
+        input4 :
+        <input type="text" name="input4" onChange={handlechange} />
+        <br />
+        input5 :
+        <input type="text" name="input5" onChange={handlechange} />
+      </form>
+      <button onClick={console.log(input2)}>print</button>
+
+      <form action="">
+        nama :
+        <input type="text" name="nama" onChange={handleData} />
+        <br />
+        alamat :
+        <input type="text" name="alamat" onChange={handleData} />
+        <br />
+        no hp :
+        <input type="text" name="nohp" onChange={handleData} />
+        <br />
+      </form>
+      <button onClick={console.log(data)}>print</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
