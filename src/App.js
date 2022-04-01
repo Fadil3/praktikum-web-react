8be9eba1c2da3851ec6d5bbf4e116e0e27c1dec6
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-01 08:29:31 +0700

    example component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
+const Greet = (props) => <h1>Good Morning, {props.name}</h1>
+
+const Template = (props) => (
+  <div style={{ color: 'green' }}>{props.children}</div>
+)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome name="john" />
+      <Greet name="john" />
+      <Template>
+        <Welcome name="john" />
+        <Greet name="john" />
+      </Template>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
